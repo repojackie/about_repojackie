f55fada29fdb45e493ce622100f7c4fe067ec1ff
Jackie <repojackie@gmail.com>
2020-08-23 17:09:55 -0400

    Now adding posts to a new DB - need to configure so that most recent posts are shown

--- README.md
+++ README.md
@@ -2,16 +2,11 @@
 A simple portfolio website for displaying information all about Jackie
 
 # TO-DO
-[] Mainpage
-	[] Navigation bar 
-	[] Blurb about me 
-[] Portfolio page
-	[] Navigation bar
-	[] External links
-[] Chat room (?)
-	[*] Flask + SocketIO -> https://codeburst.io/building-your-first-chat-application-using-flask-in-7-minutes-f98de4adfa5d
-[] Contacts/outlook
-
+- CRUD for blog posts under logged in user 
+	- User can post to the database
+	- The database is served to the website
+- Add information for respective pages
+- Format/make things look nicer
 
 # Information about this repository
 You can ignore the directory called "about_repojackie" -- this is just a python virtual environment. 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Used tutorials from PrettyPrinted among other to learn about WTForms
 TO-DO: 
     [] Backend for blog - Create an account and allow people to post on certain circumstances? 
 """
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from flask_socketio import SocketIO
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
@@ -14,7 +14,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_bootstrap import Bootstrap
-
+from datetime import datetime
 
 """
 I will look into splitting this into separate files as times goes on-- look up Flask blueprinting?
@@ -29,12 +29,21 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 socketio = SocketIO(app)
 
-
+# associated with each users identity
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+
+# for when a user decides to post
+class BlogPosts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    true_author = db.Column(db.String(80), unique=True, nullable=False)
+    author = db.Column(db.String(30), unique=True, nullable=False)
+    blurb = db.Column(db.Text(), nullable=False)
+    time = db.Column(db.DateTime(), default=datetime.utcnow)
 
 db.create_all()
 db.session.commit()
@@ -76,9 +85,20 @@ def blog():
     return render_template("blog.html")
 
 # for editing new posts
-@app.route("/blog/new_post")
+@app.route("/blog/new_post", methods=["GET", "POST"])
 @login_required
 def blog_new_post():
+    if request.method == "POST":
+        title = request.form['title']
+        author = request.form['author']
+        blurb = request.form['blurb']
+        
+        new_post = BlogPosts(title=title, author=author, blurb=blurb, true_author=current_user.username)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for("blog"))
     return render_template("blog_new_post.html", name=current_user.username)
 
 # sign up for blog posting 
